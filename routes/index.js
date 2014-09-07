e70263d90ce6f8656da65eeea73609e7dd05e142
Vagios Vlachos <bagios234@gmail.com>
2014-09-08 00:08:22 +0300

    Create route GET post

--- routes/index.js
+++ routes/index.js
@@ -33,4 +33,22 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+// Load post from db by id
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+// GET post route by id
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 module.exports = router;
