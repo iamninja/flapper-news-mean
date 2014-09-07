d7f04b4e6f6ee5296ada551c7c0671fd0dcd4069
Vagios Vlachos <bagios234@gmail.com>
2014-09-07 22:46:01 +0300

    Import previous written code

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,73 @@
+angular.module('flapperNews', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }])
+.factory('posts', [function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4},
+    ]
+  };
+  return o;
+}])
+.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+  $scope.posts = posts.posts;
+
+  $scope.addPost = function(){
+    if ($scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great but wrong!', upvotes: 0}
+      ]
+    });
+    $scope.title = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+}])
+.controller('PostsCtrl', ['$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+
+  $scope.addComment = function(){
+    if ($scope.body === '') {
+      return;
+    }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}]);
