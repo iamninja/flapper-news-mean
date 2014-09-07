0844ca02e477ccf1a65781a36d4e1394c3b5562c
Vagios Vlachos <bagios234@gmail.com>
2014-09-07 23:09:02 +0300

    Create GET and POST routes for posts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+// GET posts route
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(posts);
+  });
+});
+
+// POST posts route
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+
+    res.json(post);
+  });
 });
 
 module.exports = router;
